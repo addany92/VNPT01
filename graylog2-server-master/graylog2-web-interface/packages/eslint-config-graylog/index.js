f6e499dd83e13550b5b63c8153e38ecc6758339d
addany92 <holicgale@gmail.com>
2019-05-22 19:53:46 +0700

    gl2

---
+++ graylog2-server-master/graylog2-web-interface/packages/eslint-config-graylog/index.js
@@ -0,0 +1,32 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'airbnb',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/react',
+  ],
+  plugins: [
+    'import',
+  ],
+  rules: {
+    'arrow-body-style': 'off',
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-unresolved': 'off',
+    'max-len': 'off',
+    'new-cap': 'off',
+    'no-else-return': 'warn',
+    'no-nested-ternary': 'warn',
+    'no-underscore-dangle': 'off',
+    'object-curly-newline': ['error', { multiline: true, consistent: true }],
+    'object-shorthand': ['error', 'methods'],
+    'react/forbid-prop-types': 'off',
+    'react/jsx-closing-bracket-location': ['warn', 'after-props'],
+    'react/jsx-first-prop-new-line': ['warn', 'never'],
+    'react/jsx-indent-props': ['error', 'first'],
+    'react/jsx-one-expression-per-line': 'off',
+    'react/prefer-es6-class': 'off',
+    'react/prefer-stateless-function': 'warn',
+  },
+};
